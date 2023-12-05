4a1ad8c15815a525473f748ca927006381ef6e05
faizcode24 <mdfaiz589@gmail.com>
2023-12-05 19:16:32 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+    Authorization: "bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
+
+
+// to run on the local host we should use the run dev as a command for it.
+
+
+// woring on the new code for the rest api/ test api
+
+
+// import React from "react";
+
+// const BASE_URL = "";  // paste the new baseurl over there
